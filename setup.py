a554bd5765ff16027c6c7dd193a8a61aa836b680
DominikPott <d_tronic_p@gmx.de>
2017-06-24 14:44:18 +0200

    moved tests folder into correct position to work with travis ci added setup.py to distribute package with pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='tasker',
+    url='https://github.com/DominikPott/tasker',
+    author='Dominik Pott',
+    author_email='dominikpott@gmail.com',
+    version='0.1.0',
+    packages=['tasker',],
+    license='GNU GENERAL PUBLIC LICENSE Version 3',
+    description='A taskmanager for animaton and vfx projects.',
+    keywords='taskmanager maya houdini',
+    long_description=open('README.md').read(),
+)

f8e32db4c16a5e68e7fe48162068cc05299c1f5d
DominikPott <d_tronic_p@gmx.de>
2017-06-24 17:51:34 +0200

    fixing tasker module imports to work for python 3

--- tasker/__init__.py
+++ tasker/__init__.py
@@ -1,16 +1,18 @@
 import logging
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from tasker.model import Base
+from tasker.db_config import database
+
 __author__ = 'Dominik'
 
 FORMAT = "%(filename)s:%(funcName)s - %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model import Base
-from db_config import database
-
 engine = create_engine(database)
 Base.metadata.bind = engine
 Session = sessionmaker(bind=engine)
+
